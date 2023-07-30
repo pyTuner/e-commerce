8bc3c1ec38c3150fe00c2899c71433dd1002c8a5
Tejas <pytuner@outlook.com>
2023-07-30 22:16:43 +0530

    File: Client[frontend] | added navbar, footer, routing

---
+++ client/src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Nav = () => {
+    return (
+        <div>
+            <ul className='nav-ul'>
+                <li><Link to='/'>Products</Link></li>                      {/*  do not add anchor tag (<a>) >> it refreshes the page */}
+                <li><Link to='/add'>Add Product</Link></li> 
+                <li><Link to='/update'>Update Product</Link></li> 
+                <li><Link to='/profile'>Profile</Link></li> 
+                <li><Link to='/logout'>Logout</Link></li> 
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
